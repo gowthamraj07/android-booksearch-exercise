f33aa9e79004d9f7dbcba259e4e7e69ea5dd69db
Gowtham Shanmugaraj <gowthamraj07@gmail.com>
2020-07-20 14:33:56 +0200

    Create first working Espresso test case

--- app/src/androidTest/java/com/codepath/android/booksearch/ApplicationTest.java
+++ app/src/androidTest/java/com/codepath/android/booksearch/ApplicationTest.java
@@ -1,14 +1,36 @@
 package com.codepath.android.booksearch;
 
-import android.app.Application;
+import android.widget.TextView;
 
+import com.codepath.android.booksearch.activities.BookListActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withResourceName;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.instanceOf;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class ApplicationTest {
 
+    @Rule
+    public ActivityTestRule<BookListActivity> activityRule = new ActivityTestRule<>(BookListActivity.class);
+
+    @Test
+    public void check_BookListActivity() {
+        onView(allOf(instanceOf(TextView.class),
+                withParent(withResourceName("action_bar"))))
+                .check(matches(withText("BookSearch")));
+    }
 }
