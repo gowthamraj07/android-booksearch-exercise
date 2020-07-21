eda976239ccc5d8e51871547873b0ff827b83197
Gowtham Shanmugaraj <gowthamraj07@gmail.com>
2020-07-21 23:11:45 +0200

    Add navigation to BookListActivity

--- app/src/main/java/com/codepath/android/booksearch/activities/SignInActivity.java
+++ app/src/main/java/com/codepath/android/booksearch/activities/SignInActivity.java
@@ -1,6 +1,9 @@
 package com.codepath.android.booksearch.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.codepath.android.booksearch.R;
 
@@ -12,5 +15,14 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        Button btnSignIn = findViewById(R.id.btnSignIn);
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignInActivity.this, BookListActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
