aecc0ace87725878a2d2b2331ce02ed03e725d1d
Gowtham Shanmugaraj <gowthamraj07@gmail.com>
2020-07-21 23:07:10 +0200

    Add login screen

---
+++ app/src/main/java/com/codepath/android/booksearch/activities/SignInActivity.java
@@ -0,0 +1,16 @@
+package com.codepath.android.booksearch.activities;
+
+import android.os.Bundle;
+
+import com.codepath.android.booksearch.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
